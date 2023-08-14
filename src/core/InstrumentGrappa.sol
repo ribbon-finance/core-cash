ef0028263c3e32ab900459dbe16f5fbb3421be6d
Your Name <jacob4seinfeld@gmail.com>
2023-08-14 10:23:21 +0800

    add instrument grappa boilerplate

---
+++ src/core/InstrumentGrappa.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// imported contracts and libraries
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+
+import {SafeCast} from "openzeppelin/utils/math/SafeCast.sol";
+
+// interfaces
+import {ICashOptionToken} from "../interfaces/ICashOptionToken.sol";
+
+// libraries
+import {BalanceUtil} from "../libraries/BalanceUtil.sol";
+import {MoneynessLib} from "../libraries/MoneynessLib.sol";
+import {NumberUtil} from "../libraries/NumberUtil.sol";
+import {ProductIdUtil} from "../libraries/ProductIdUtil.sol";
+import {TokenIdUtil} from "../libraries/TokenIdUtil.sol";
+import {InstrumentIdUtil} from "../libraries/InstrumentIdUtil.sol";
+
+// constants and types
+import "../config/types.sol";
+import "../config/enums.sol";
+import "../config/constants.sol";
+import "../config/errors.sol";
+
+import {Grappa} from "./Grappa.sol";
+
+contract InstrumentGrappa is Grappa {
+    using BalanceUtil for Balance[];
+    using FixedPointMathLib for uint256;
+    using NumberUtil for uint256;
+    using ProductIdUtil for uint40;
+    using SafeCast for uint256;
+    using TokenIdUtil for uint256;
+
+    /// @dev optionToken address
+    ICashOptionToken public immutable instrumentToken;
+
+    /*///////////////////////////////////////////////////////////////
+                       State Variables V1
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev instrumentId => instrument
+    mapping(uint256 => Instrument) public instruments;
+
+    event InstrumentRegistered(uint256 id);
+
+    /*///////////////////////////////////////////////////////////////
+              Constructor for implementation Contract
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev set immutables in constructor
+    /// @dev also set the implementation contract to initialized = true
+    constructor(address _optionToken, address _instrumentToken) Grappa(_optionToken) initializer {
+        instrumentToken = ICashOptionToken(_instrumentToken);
+    }
+
+    /**
+     * @dev register an instrument
+     * @param _instrument Instrument to register
+     * @return id instrument ID
+     */
+    function registerInstrument(Instrument calldata _instrument) external returns (uint256 id) {
+        id = InstrumentIdUtil.getInstrumentId(_instrument);
+
+        if (instruments[id].options.length == 0) revert GP_InstrumentAlreadyRegistered();
+
+        instruments[id] = _instrument;
+
+        emit InstrumentRegistered(id);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                          External Functions
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev parse instrument id into composing features, coupons, options
+     * @param _instrumentId instrument id`
+     */
+    function getDetailFromInstrumentId(uint256 _instrumentId)
+        public
+        view
+        returns (uint40 autocallId, uint256 coupons, Option[] memory options)
+    {
+        Instrument memory _instrument = instruments[_instrumentId];
+        return (_instrument.autocallId, _instrument.coupons, _instrument.options);
+    }
+
+    /**
+     * @dev get instrument id from autocall id, coupons, options array
+     * @dev       function will still return even if instrument is not registered
+     * @param _instrument Instrument
+     * @return id instrument ID
+     */
+    function getInstrumentId(Instrument calldata _instrument) external pure returns (uint256 id) {
+        id = InstrumentIdUtil.getInstrumentId(_instrument);
+    }
+}
