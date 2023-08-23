8b6bb54a074ddc5abf276b8e1cdaec3a790a4924
Sahilgat <38809578+Sahilgat@users.noreply.github.com>
2023-08-23 17:55:11 +0800

    wip

--- src/core/oracles/PythOracleDisputable.sol
+++ src/core/oracles/PythOracleDisputable.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
+
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
@@ -29,24 +32,24 @@ contract PythOracleDisputable is IOracle, Ownable {
         uint128 price;
     }
 
-    // base => quote => timestamp => price.
-    mapping(address => mapping(address => mapping(uint256 => HistoricalPrice))) public historicalPrices;
+    // asset => timestamp => price.
+    mapping(address => mapping(uint256 => HistoricalPrice)) public historicalPrices;
 
-    // base => quote => dispute period
-    mapping(address => mapping(address => uint256)) public disputePeriod;
+    // asset => dispute period
+    mapping(address => uint256) public disputePeriod;
 
-    // asset => pyth price feed IDs (https://pyth.network/developers/price-feed-ids)
-    mapping(address => bytes32) public priceFeedIds;
+    // pyth price feed IDs (https://pyth.network/developers/price-feed-ids) => asset
+    mapping(bytes32 => address) public priceFeedIds;
 
     /*///////////////////////////////////////////////////////////////
                                  Events
     //////////////////////////////////////////////////////////////*/
 
-    event HistoricalPriceSet(address base, address quote, uint256 timestamp, uint256 price, bool isDispute);
+    event HistoricalPriceSet(address asset, uint256 timestamp, uint256 price, bool isDispute);
 
-    event DisputePeriodUpdated(address base, address quote, uint256 period);
+    event DisputePeriodUpdated(address asset, uint256 period);
 
-    event PriceFeedIDUpdated(address asset, bytes32 id);
+    event PriceFeedIDUpdated(bytes32 id, address asset);
 
     event PythUpdated(address newPyth);
 
@@ -80,10 +83,11 @@ contract PythOracleDisputable is IOracle, Ownable {
         view
         returns (uint256 price, bool isFinalized)
     {
-        HistoricalPrice memory data = historicalPrices[_base][_quote][_timestamp];
-        if (data.reportAt == 0) revert OC_PriceNotReported();
+        // TODO need to do internal conversion from base to quote
+        // HistoricalPrice memory data = historicalPrices[_base][_quote][_timestamp];
+        // if (data.reportAt == 0) revert OC_PriceNotReported();
 
-        return (data.price, _isPriceFinalized(_base, _quote, _timestamp));
+        // return (data.price, _isPriceFinalized(_base, _quote, _timestamp));
     }
 
     /**
@@ -96,19 +100,21 @@ contract PythOracleDisputable is IOracle, Ownable {
     /**
      * @dev view function to check if dispute period is over
      */
-    function isPriceFinalized(address _base, address _quote, uint256 _timestamp) external view returns (bool) {
-        return _isPriceFinalized(_base, _quote, _timestamp);
+    function isPriceFinalized(address _asset, uint256 _timestamp) external view returns (bool) {
+        return _isPriceFinalized(_asset, _timestamp);
     }
 
     /**
      * @notice report pyth price at a timestamp and write to storage
      * @dev anyone can call this function and set the price for a given timestamp
      */
-    function reportPrice(address _base, address _quote, uint256 _timestamp, bytes[] calldata pythUpdateData)
-        external
+    function reportPrice(bytes[] calldata _pythUpdateData, bytes32[] calldata priceIds, uint256 _timestamp)
+        external payable
     {
         if (_timestamp > block.timestamp) revert OC_CannotReportForFuture();
         if (historicalPrices[_base][_quote][_timestamp].reportAt != 0) revert OC_PriceReported();
+        uint updateFee = pyth.getUpdateFee(_pythUpdateData);
+        PythStructs.PriceFeed[] memory priceFeeds = pyth.parsePriceFeedUpdates{value: updateFee}(_pythUpdateData, priceIds, _timestamp);
 
         (uint256 basePrice, uint8 baseDecimals) = _getLastPriceBeforeExpiry(_base, _baseRoundId, _timestamp);
         (uint256 quotePrice, uint8 quoteDecimals) = _getLastPriceBeforeExpiry(_quote, _quoteRoundId, _timestamp);
@@ -125,34 +131,33 @@ contract PythOracleDisputable is IOracle, Ownable {
 
     /**
      * @dev dispute a reported price from the owner. Cannot dispute an un-reported price
-     * @param _base base asset
-     * @param _quote quote asset
+     * @param _asset the address of the asset
      * @param _timestamp timestamp
      * @param _newPrice new price to set
      */
-    function disputePrice(address _base, address _quote, uint256 _timestamp, uint256 _newPrice) external onlyOwner {
-        HistoricalPrice memory entry = historicalPrices[_base][_quote][_timestamp];
+    function disputePrice(address _asset, uint256 _timestamp, uint256 _newPrice) external onlyOwner {
+        // TODO can't dispute stable asset price
+        HistoricalPrice memory entry = historicalPrices[_asset][_timestamp];
         if (entry.reportAt == 0) revert OC_PriceNotReported();
 
-        if (entry.reportAt + disputePeriod[_base][_quote] < block.timestamp) revert OC_DisputePeriodOver();
+        if (entry.reportAt + disputePeriod[_asset] < block.timestamp) revert OC_DisputePeriodOver();
 
-        historicalPrices[_base][_quote][_timestamp] = HistoricalPrice(true, uint64(block.timestamp), _newPrice.safeCastTo128());
+        historicalPrices[_asset][_timestamp] = HistoricalPrice(true, uint64(block.timestamp), _newPrice.safeCastTo128());
 
-        emit HistoricalPriceSet(_base, _quote, _timestamp, _newPrice, true);
+        emit HistoricalPriceSet(_asset, _timestamp, _newPrice, true);
     }
 
     /**
      * @dev set the dispute period for a specific base / quote asset
-     * @param _base base asset
-     * @param _quote quote asset
-     * @param _period dispute period. Cannot be set to a value longer than 6 hours
+     * @param _asset the address of the asset
+     * @param _period dispute period. Cannot be set to a value longer than 365 days
      */
-    function setDisputePeriod(address _base, address _quote, uint256 _period) external onlyOwner {
+    function setDisputePeriod(address _asset, uint256 _period) external onlyOwner {
         if (_period > MAX_DISPUTE_PERIOD) revert OC_InvalidDisputePeriod();
 
-        disputePeriod[_base][_quote] = _period;
+        disputePeriod[_asset] = _period;
 
-        emit DisputePeriodUpdated(_base, _quote, _period);
+        emit DisputePeriodUpdated(_asset, _period);
     }
 
     /**
@@ -164,12 +169,12 @@ contract PythOracleDisputable is IOracle, Ownable {
         if (_asset == address(0)) revert OC_ZeroAddress();
         if (_id == bytes32(0)) revert PY_InvalidPriceFeedID();
 
-        bytes32 currentID = priceFeedIds[_asset];
-        if (currentID == _id) revert OC_ValueUnchanged();
+        bytes32 currentAsset = priceFeedIds[_id];
+        if (currentAsset == _asset) revert OC_ValueUnchanged();
 
-        priceFeedIds[_asset] = _id;
+        priceFeedIds[_id] = _asset;
 
-        emit PriceFeedIDUpdated(_asset, _id);
+        emit PriceFeedIDUpdated(_id, _asset);
     }
 
     /**
@@ -190,13 +195,14 @@ contract PythOracleDisputable is IOracle, Ownable {
      * @dev checks if dispute period is over
      *      if true, getPriceAtTimestamp will return (price, true)
      */
-    function _isPriceFinalized(address _base, address _quote, uint256 _timestamp) internal view override returns (bool) {
-        HistoricalPrice memory entry = historicalPrices[_base][_quote][_timestamp];
+    function _isPriceFinalized(address _asset, uint256 _timestamp) internal view override returns (bool) {
+        // TODO if stable asset always return true
+        HistoricalPrice memory entry = historicalPrices[_asset][_timestamp];
         if (entry.reportAt == 0) return false;
 
         if (entry.isDisputed) return true;
 
-        return block.timestamp > entry.reportAt + disputePeriod[_base][_quote];
+        return block.timestamp > entry.reportAt + disputePeriod[_asset];
     }
 
     /**
